34cf9acf01714f8bc9307fd9fa2f33f460a38dcd
basith.razak <basith.razak@piserve.com>
2026-02-17 20:08:26 +0530

    feat: changed git root

--- src/main/java/com/masterglobal/erp/repository/ChargeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.masterglobal.erp.repository;
-
-import com.masterglobal.erp.entity.Charge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChargeRepository extends JpaRepository<Charge, Long> {
-}
--- src/main/java/com/masterglobal/erp/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.masterglobal.erp.repository;
-
-import com.masterglobal.erp.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
